8253742fb7a85601f0744bca458524e8bb84eba8
waymao <my.waymao+git@gmail.com>
2022-12-18 13:12:23 -0500

    new experiments for MNIST

--- get_results.py
+++ get_results.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import pandas as pd
+import json
 
 def get_csv(file_list):
     for file in file_list:
@@ -8,7 +9,11 @@ def get_csv(file_list):
             return file
     return ""
 
-path = "results/run1/"
+def get_read_config(dir):
+    with open(os.path.join(dir, "params.json"), 'r') as f:
+        return json.load(f)
+
+path = "results/"
 dirs = os.listdir(path)
 
 df = None
@@ -20,15 +25,22 @@ for dir in dirs:
     if os.path.isdir(d):
         contents = os.listdir(d)
         csv_file = get_csv(contents)
-        data = pd.read_csv(os.path.join(d, csv_file))
+        if csv_file == "":
+            continue
+        config = get_read_config(d)
+        curr_run_data = pd.read_csv(os.path.join(d, csv_file))
 
         if df is None:
-            df = pd.DataFrame(columns=['run_name', *data.columns])
+            df = pd.DataFrame(columns=['run_name', 'type', 'lambda_bt', 'n_dims_code', *curr_run_data.columns])
             df['run_name'] = df['run_name'].astype('str')
+            df['type'] = df['type'].astype('str')
         
-        last_stat = data.iloc[-1]
+        last_stat = curr_run_data.iloc[-1].copy()
         last_stat['run_name'] = dir
-        print(d)
+        last_stat['type'] = config['method']
+        last_stat['lambda_bt'] = config['lambda_bt']
+        last_stat['n_dims_code'] = config['n_dims_code']
+        print("Processed", d)
         df.loc[len(df.index)] = last_stat
         # print(f"{dir:>40} | {last_te_bce_error}")
 
@@ -38,6 +50,9 @@ if df is None:
 
 df = df.sort_values(by=["te_bce_error"])
 df = df.reset_index(drop=True)
+pd.set_option('display.max_rows', None)
+print()
+print("                   ******** Table of results, sorted by test bce error ********")
 print(df)
 df.to_csv("results_report.csv", index=False)
 
--- grid_search_config.py
+++ grid_search_config.py
@@ -1,12 +1,22 @@
 CMD = ["python3", "training_on_mnist.py"]
 
+# PARAMS_TO_SEARCH = {
+#     "method": ["VAEBT"],
+#     "n_epochs": [200],
+#     "lr": [0.005],
+#     "n_dims_code": [2, 16, 64, 128],
+#     "n_mc_samples": [10],
+#     "lambda_bt": [0.25, 0.025, 0.0025, 0.00025, -0.00025],
+#     "hidden_layer_sizes": [32, 64, 128]
+# }
+
 PARAMS_TO_SEARCH = {
-    "method": ["VAEBT"],
+    "method": ["VAE"],
     "n_epochs": [200],
     "lr": [0.005],
     "n_dims_code": [2, 16, 64, 128],
     "n_mc_samples": [10],
-    "lambda_bt": [0.25, 0.025, 0.0025, 0.00025, -0.00025],
+    "lambda_bt": [0],
     "hidden_layer_sizes": [32, 64, 128]
 }
 
